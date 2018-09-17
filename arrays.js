e9cec1b5c357f0e31b508372c4df38a83d72bfc6
Connor Finnegan <connorfinnegan16@gmail.com>
2018-09-17 16:38:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = originalArray;
   return array.unshift(element);
 }
 
