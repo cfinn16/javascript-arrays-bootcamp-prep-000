c8a3aa8c10f71abe7ce071ea61cd09729c9cacb4
Connor Finnegan <connorfinnegan16@gmail.com>
2018-09-17 15:31:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ function addElementToBeginningOfArray(array, element) {
   var newArray = [element, ...array];
   return newArray
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
